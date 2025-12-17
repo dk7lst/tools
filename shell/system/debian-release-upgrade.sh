9e93d6281e08ba35e99fde5ffcb31ad78c1b7f3e
Lars Struss <mail@lsnet.eu>
2025-12-17 16:49:48 +0100

    "debian-release-upgrade.sh" updated for upgrade from Debian 12 (bookworm) to Debian 13 (trixie).

--- shell/system/debian-release-upgrade.sh
+++ shell/system/debian-release-upgrade.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+
+# https://www.debian.org/releases/trixie/release-notes/upgrading.en.html
+# https://www.debian.org/releases/trixie/release-notes/issues.en.html
+
 lsb_release -a
 uname -mrs
 cat /etc/os-release
 cat /etc/debian_version
 
-sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
+sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
 
 # TODO: Edit files in "/etc/apt/sources.list.d" as needed!
 
